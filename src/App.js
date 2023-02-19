eba429d0d8e417c970702547e5f62d3b9af17d4a
zubayer-salehin <freelancerzubear@gmail.com>
2023-02-19 15:32:01 +0600

    Environment set up done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import routes from './routes/routes';
+
+function App() {
+
+  return (
+    <div>
+      <RouterProvider router={routes} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+
+
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    }
+])
+
+export default routes;
